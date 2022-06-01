72acb5aa204b2662434a8bd6c5605514ebd19f0e
Kuss <kussssss@outlook.com>
2022-06-01 20:25:39 +0800

    feat: initial

---
+++ theme.config.js
@@ -0,0 +1,27 @@
+// theme.config.js
+export default {
+  projectLink: 'https://github.com/ncuhome/mincu-site', // GitHub link in the navbar
+  docsRepositoryBase: 'https://github.com/ncuhome/mincu-site/blob/master', // base URL for the docs repository
+  titleSuffix: ' – Mincu',
+  nextLinks: true,
+  prevLinks: true,
+  search: false,
+  customSearch: null, // customizable, you can use algolia for example
+  darkMode: true,
+  footer: true,
+  footerText: `MIT ${new Date().getFullYear()} © NCUHOME.`,
+  footerEditLink: `Edit this page on GitHub`,
+  logo: (
+    <>
+      <svg></svg>
+      <span>南大家园 WEB JS API</span>
+    </>
+  ),
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="description" content="Mincu" />
+      <meta name="og:title" content="Mincu" />
+    </>
+  ),
+}
